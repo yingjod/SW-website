42a368ec701561472c77b5b8de8fd3fa8742822c
Ying L <forgmina@gmail.com>
2024-04-19 10:32:12 +0100

    set up most of the pages

--- ShinWang/ShinWang/settings.py
+++ ShinWang/ShinWang/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'dashboard',
     'item',
+    'conversation',
     'core',
 ]
 
--- ShinWang/core/admin.py
+++ ShinWang/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from conversation.models import Conversation, ConversationMessage
+
+admin.site.register(Conversation)
+admin.site.register(ConversationMessage)
